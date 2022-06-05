93fe5e4523740b13d6ad3df88e99541c638e3a3e
ummuhan kurt <ummuhankurt.23@outlook.com>
2022-06-05 14:19:05 +0300

    Add project files.

---
+++ Mvc/Controllers/EmployeeController.cs
@@ -0,0 +1,56 @@
+﻿using Mvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            IEnumerable<mvcEmployeeModel> empList;
+            HttpResponseMessage response = GlobalVeriables.WebApiClient.GetAsync("Employee").Result;
+            empList = response.Content.ReadAsAsync<IEnumerable<mvcEmployeeModel>>().Result;
+            return View(empList);
+        }
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            if(id == 0)
+            {
+                return View(new mvcEmployeeModel());
+            }
+            else
+            {
+                HttpResponseMessage response = GlobalVeriables.WebApiClient.GetAsync("Employee/" + id.ToString()).Result;
+                return View(response.Content.ReadAsAsync<mvcEmployeeModel>().Result);
+            }
+            
+        }
+        [HttpPost]
+        public ActionResult AddOrEdit(mvcEmployeeModel emp)
+        {
+            if(emp.EmployeeID == 0)
+            {
+               HttpResponseMessage response = GlobalVeriables.WebApiClient.PostAsJsonAsync("Employee", emp).Result;
+               TempData["SuccessMessage"] = "Kaydedildi";
+            }
+            else
+            {
+                HttpResponseMessage response = GlobalVeriables.WebApiClient.PutAsJsonAsync("Employee/" + emp.EmployeeID, emp).Result;
+                TempData["SuccessMessage"] = "Güncellendi";
+            }
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(int id)
+        {
+            HttpResponseMessage response = GlobalVeriables.WebApiClient.DeleteAsync("Employee/" + id.ToString()).Result;
+            TempData["SuccessMessage"] = "Silindi";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Mvc/GlobalVeriables.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+
+namespace Mvc
+{
+    public static class GlobalVeriables
+    {
+        public static HttpClient WebApiClient = new HttpClient();
+        static GlobalVeriables()
+        {
+            WebApiClient.BaseAddress = new Uri("https://localhost:44381/api/");
+            WebApiClient.DefaultRequestHeaders.Clear();
+            WebApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
